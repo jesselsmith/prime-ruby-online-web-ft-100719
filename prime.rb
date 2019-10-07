4861f8e033a6176767f4e7750eb2fa0f2bcb004b
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-07 20:22:12 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime?(int)
   if int < 0
-    divisors = 2...((-int)/2)
+    false
   else
-    divisors = 2...(int/2)
-  end
+  divisors = 2...(int/2)
   divisors.none? do |divisor|
     int % divisor == 0
   end

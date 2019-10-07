f2438cc18d548c3e00f2758cc102b3fac04ced72
Jesse Smith <jesse.lee.smith@gmail.com>
2019-10-07 20:20:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def prime?(int)
   if int < 0
     divisors = 2...((-int)/2)
@@ -7,7 +5,6 @@ def prime?(int)
     divisors = 2...(int/2)
   end
   divisors.none? do |divisor|
-    binding.pry
     int % divisor == 0
   end
 end
